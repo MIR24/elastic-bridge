fcfae79da205b107d042b37428f283da683a4ce9
Konshin Alexander <kon2work@gmail.com>
2017-11-22 17:27:07 +0300

    fix php path

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-/usr/bin/php run/run_index.php;
-/usr/bin/php run/run_index_update.php;
-/usr/bin/php run/run_index_delete.php;
+/usr/local/bin/php run/run_index.php;
+/usr/local/bin/php run/run_index_update.php;
+/usr/local/bin/php run/run_index_delete.php;
